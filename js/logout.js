b85ec5304d121afdb3b49d425eb04ea99bb9c2f5
unknown <18401644217@163.com>
2017-06-08 22:51:15 +0800

    登出弹窗

--- js/logout.js
+++ js/logout.js
@@ -1,9 +1,11 @@
- $('a[id=log]').on('click', function() {
+ $('button[id=log]').on('click', function() {
      var d = dialog({
-         content: '是否登出',
+         content: '是否登出?',
          okValue: '是',
+         ok: function() {},
          quickClose: true,
          cancelValue: '否',
+         cancel: function() {},
      });
      d.show();
  });
